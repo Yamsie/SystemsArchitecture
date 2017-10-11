bfaffed3b6c72d76c9e340a8f72ce4786c43a935
Jiasen Tian <16006245@studentmail.ul.ie>
2017-10-11 23:16:00 +0100

    test file to show how to use interceptor

---
+++ src/bll/models/logger/test.java
@@ -0,0 +1,19 @@
+package bll.models.logger;
+
+public class test {
+    public static void test(){
+        LoggerOperator operator = new LoggerOperator("src/bll/models/logger/log.txt");
+        Interceptor dateInterceptor = new DateInterceptor();
+        Interceptor databaseInterceptor = new DatabaseTestingLineInterceptor();
+        Interceptor statementInterceptor = new TestingStatementInterceptor();
+        InterceptorDispatcher actionInvocation = new InterceptorDispatcher();
+        actionInvocation.addInterceptor(dateInterceptor);
+        actionInvocation.addInterceptor(databaseInterceptor);
+        actionInvocation.addInterceptor(statementInterceptor);
+        Action action = new TestingAction();
+        actionInvocation.setAction(action);
+        String result = actionInvocation.invoke();
+        operator.addData(result + "\n");
+        System.out.println("Action result:" + result);
+    }
+}
